9ab04cd96ce91198994d2a120913a4b57c1e4900
Dennis2340 <kamaradennis36@gmail.com>
2023-09-19 16:40:57 +0000

    testing the newmaster

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -1,6 +1,11 @@
 const express = require("express")
 const router = express.Router()
 const userController = require("../Controller/userController")
+const handleHeaderError = require("../middleware/handlesHeaderError");
+const multer = require("multer")
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 router.get("/getUserInfo", userController.getUserInfo)
 router.post("/register",upload.single("picture"), handleHeaderError,userController.addNewUser)
